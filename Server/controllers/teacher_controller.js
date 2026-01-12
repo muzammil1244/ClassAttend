b9ac71bfa1ffead5037717f0f7ded7def6ed9cbf
Muzammil <muzammil844641@gmail.com>
2026-01-12 23:03:11 +0530

    feat(Server) add some feature like student score class score

--- Server/controllers/teacher_controller.js
+++ Server/controllers/teacher_controller.js
@@ -1,4 +1,5 @@
 
+import { json } from "express"
 import pool from "../connections/db_connent.js"
 // CRUD OPERATION OF WITH STUDENT
 
@@ -14,7 +15,9 @@ export const delete_student = (req, res) => {
     return res.send("delete student ")
 }
 
-
+export const read_student = (req, res) => {
+    return res.send("read student ")
+}
 
 // ATTENDANCE MANAGEMENT
 
@@ -55,15 +58,6 @@ export const teacher_classes = async (req, res) => {
 
 }
 
-
-export const read_student = (req, res) => {
-    return res.send("read student ")
-}
-
-export const see_particular_student_scores = () => {
-    return res.send("student score")
-}
-
 export const create_attendance = async (req, res) => {
 
     let teacher_id = req.user?.id
@@ -101,6 +95,105 @@ ON DUPLICATE KEY UPDATE
     } catch (error) {
         return res.status(500).json({
             message: "error from create attendance",
+            error
+        })
+    }
+}
+
+
+export const see_particular_student_scores = async (req, res) => {
+    let teacher_id = req.user?.id
+    let student_id = req.params?.id
+    let subject_id = req.params?.sub
+    try {
+
+        if (!teacher_id || !student_id) {
+            return res.status(400).json({
+                message: "all field required"
+            })
+        }
+
+        let sql_student = `
+        SELECT * FROM student_db WHERE id = ?
+        `
+        let [confirm_studnet] = await pool.query(sql_student, [student_id])
+
+
+
+        let sql_sub = `
+SELECT * FROM subject_db WHERE id = ?
+`
+
+        let [confirm_sub] = await pool.query(sql_sub, [subject_id])
+
+        if (confirm_studnet.length < 1 || confirm_sub.length < 1) {
+            return res.status(401).json({
+                message: "student or subject are not founded in db"
+            })
+        }
+
+
+
+
+        let sql_score = `
+SELECT COUNT(*) AS total ,
+SUM(CASE WHEN status ='P' THEN 1 ELSE 0 END ) AS present_classes 
+ from att_db WHERE teacher_id = ? AND subject_id = ? AND student_id =?
+
+`
+        let [result] = await pool.query(sql_score, [teacher_id, subject_id, student_id])
+
+        let total = result[0].total
+        let present = result[0].present_classes
+
+        let percentage = (present / total) * 100
+        console.log(typeof (total), total, " |", typeof (present), present, percentage)
+        return res.status(200).json({
+            message: "att score collect successfully",
+            percentage
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "error from the read student scores ",
+            error
+        })
+    }
+}
+
+export const see_particular_subject_score = async (req, res) => {
+
+
+    try {
+        let subject_id = req.params?.sub
+        let class_id = req.params?.class
+
+        let sql = `
+        SELECT 
+COUNT(DISTINCT s.id) AS total_students,
+COUNT(DISTINCT a.att_date) AS total_classes,
+SUM(CASE WHEN a.status = 'P' THEN 1 ELSE 0 END) AS total_presents,
+(
+  SUM(CASE WHEN a.status = 'P' THEN 1 ELSE 0 END) /
+  (COUNT(DISTINCT s.id) * COUNT(DISTINCT a.att_date))
+) * 100 AS presenty_percentage
+FROM student_db s
+JOIN att_db a ON s.id = a.student_id
+WHERE s.class_id = ?
+AND a.subject_id = ?;
+
+        `
+
+        let data = await pool.query(sql,[class_id,subject_id])
+
+        return res.status(200).json({
+            message:"data collected successfully",
+            data
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "error from see_particular_subject_score ",
             error
         })
     }
--- Server/routes/teacher_route.js
+++ Server/routes/teacher_route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { add_student, create_attendance, delete_student, download_student_attendance, read_all_attendance, read_particular_attendance, read_student, see_particular_student_scores, teacher_classes, update_attendance, update_student } from "../controllers/teacher_controller.js"
+import { add_student, create_attendance, delete_student, download_student_attendance, read_all_attendance, read_particular_attendance, read_student, see_particular_student_scores, see_particular_subject_score, teacher_classes, update_attendance, update_student } from "../controllers/teacher_controller.js"
 
 
 export const teacher_route = express.Router()
@@ -14,7 +14,8 @@ teacher_route.get("/red/student",read_student)
 // ADDITIONAL FEATURE
 teacher_route.get("/read/classes",teacher_classes)
 teacher_route.post("/add/attendance",create_attendance)
-teacher_route.post("/student/score",see_particular_student_scores)
+teacher_route.get("/student/score/:id/:sub",see_particular_student_scores)
+teacher_route.get("/student/subject/score/:class/:sub",see_particular_subject_score)
 teacher_route.get("/download/attendance",download_student_attendance)
 teacher_route.patch("/update/attendance",update_attendance)
 teacher_route.get("/read/all/attendance",read_all_attendance)
